dd7217602f41a979161b4295e2ff99faaf9fd784
Mohit Aswani <72434608+MohitAswani@users.noreply.github.com>
2022-05-09 19:55:52 +0530

    Initial commit module 13.

---
+++ 15_Mongoose/app.js
@@ -0,0 +1,43 @@
+const path = require('path');
+
+const express = require('express');
+
+const User = require('./models/user');
+
+const errorController = require('./controllers/error');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+const mongoConnect = require('./util/database').mongoConnect;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
+
+app.set('views', 'views');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+    User.findById('6279177a13fd9996d409bcf4')
+        .then(user => {
+            req.user=new User(user.name,user.email,user.cart,user._id); 
+            next();
+        })
+        .catch(err => {
+            console.log(err);
+        });
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.getError);
+
+mongoConnect(() => {
+    app.listen(3000);
+});
---
+++ 15_Mongoose/controllers/shop.js
@@ -0,0 +1,120 @@
+const Product = require('../models/product');
+
+exports.getProducts = (req, res, next) => {
+    Product.fetchAll()
+        .then(products => {
+            res.render('shop/product-list', {
+                prods: products,
+                pageTitle: 'All products',
+                path: '/products'
+            });
+        })
+        .catch(err => {
+            console.log(err);
+        });
+};
+
+exports.getProduct = (req, res, next) => {
+    const prodId = req.params.productId;
+    Product.findById(prodId)
+        .then(product => {
+            res.render('shop/product-details', {
+                pageTitle: product.title,
+                path: '/products',
+                product: product
+            });
+        }).catch((err) => {
+            console.log(err);
+        });
+}
+
+exports.getIndex = (req, res, next) => {
+    Product.fetchAll()
+        .then(products => {
+            res.render('shop/product-list', {
+                prods: products,
+                pageTitle: 'Shop from shop.js',
+                path: '/',
+            });
+        })
+        .catch(err => {
+            console.log(err);
+        });
+};
+
+exports.getCart = (req, res, next) => {
+    req.user.getCart()
+        .then(products => {
+            res.render('shop/cart', {
+                path: '/cart',
+                pageTitle: 'Your Cart',
+                products: products
+            })
+        })
+        .catch(err => {
+            console.log(err);
+        })
+};
+
+exports.postCart = (req, res, next) => {
+    const prodId = req.body.productId.trim();
+    Product.findById(prodId)
+        .then(product => {
+            return req.user.addToCart(product);
+        })
+        .then(result => {
+            res.redirect('/cart');
+        })
+        .catch(err => {
+            console.log(err);
+        })
+}
+
+exports.getOrders = (req, res, next) => {
+    req.user.getOrders()
+        .then(orders => {
+            res.render('shop/orders', {
+                path: '/orders',
+                pageTitle: 'Your orders',
+                orders: orders
+            });
+        })
+        .catch(err => {
+            console.log(err);
+        });
+}
+
+
+exports.getCheckout = (req, res, next) => {
+    res.render('shop/checkout', {
+        path: '/checkout',
+        pageTitle: 'Checkout'
+    })
+};
+
+exports.postDeleteCartProduct = (req, res, next) => {
+    let { id } = req.body;
+    id = id.trim();
+
+    Product.findById(id)
+        .then(product => {
+            return req.user.deleteFromCart(product);
+        })
+        .then(result => {
+            res.redirect('/cart');
+        })
+        .catch(err => {
+            console.log(err);
+        });
+};
+
+exports.postOrder = (req, res, next) => {
+    req.user.addOrder()
+        .then(result => {
+            res.redirect('/orders');
+        })
+        .catch(err => {
+            console.log(err);
+        })
+}
+
