bbe23166585f1af6fe9e4aef1ca420dca75d78df
Mohit Aswani <72434608+MohitAswani@users.noreply.github.com>
2022-05-14 15:28:39 +0530

    Initial commit module 16

---
+++ 18_NodeMailer/app.js
@@ -0,0 +1,93 @@
+const path = require('path');
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const csurf = require('csurf');
+const flash = require('connect-flash');
+
+const User = require('./models/user');
+
+const errorController = require('./controllers/error');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
+
+const MONGODB_URI = 'mongodb+srv://aswanim96:Mohit1234@cluster0.o2of3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+const app = express();
+
+const store = new MongoDBStore({
+    uri: MONGODB_URI,
+    collection: 'sessions',
+    expires: new Date(Date.now() + 24 * 60 * 60 * 1000)
+});
+
+const csrfprotection = csurf({});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(session({
+    secret: 'secret key',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+        httponly: true,
+        store: store
+    },
+    store: store
+}));
+
+app.use(csrfprotection);
+
+app.use(flash());
+
+app.set('view engine', 'ejs');
+
+app.set('views', 'views');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+    if (req.session.user) {
+
+        User.findById(req.session.user._id)
+            .then(user => {
+                req.user = user;
+                next();
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+    else {
+        next();
+    }
+});
+
+app.use((req, res, next) => {
+    res.locals.isAuth = req.session.isAuth;
+    res.locals.csrfToken = req.csrfToken();
+    next();
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use(authRoutes);
+
+app.use(errorController.getError);
+
+mongoose.connect(MONGODB_URI)
+    .then(result => {
+        console.log('Connection successful');
+        app.listen(3000);
+    })
+    .catch(err => {
+        console.log(err);
+    });
